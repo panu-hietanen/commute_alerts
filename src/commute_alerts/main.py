90ecaa12d5bca25346c0c592e3d1f24d89701648
panu-hietanen <78078779+panu-hietanen@users.noreply.github.com>
2026-01-26 22:13:24 +0000

    Use env file and read filepath from it

--- src/commute_alerts/main.py
+++ src/commute_alerts/main.py
@@ -1,17 +1,26 @@
+import os
 from pathlib import Path
+import googlemaps as gm
 
 from utils.data import read_csv
+from utils.start import load_env
 
-COORD_FILE = Path('data\latlong.csv')
 
 def main():
+    load_env()
     print("Commute Alerts Service is running...")
 
     # FORMAT: {'home': (lat, long), 'work1': (lat, long), ...}
-    data = read_csv(COORD_FILE)
-    n_work = len(data) - 1
+    coord_file = os.getenv('COORD_FILE')
+    if not coord_file:
+        raise ValueError("COORD_FILE environment variable not set. Please check your .env file.")
+    
+    coords = read_csv(coord_file)
+    n_work = len(coords) - 1
 
-    print(data)
+    gmaps = gm.Client(key=os.getenv('GMAPS_API_KEY'))
+
+    print(coords)
 
 if __name__ == "__main__":
     main()
